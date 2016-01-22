01dc620be03ca8ea11c78ce9a57027910d524742
delight26 <ljb0426@naver.com>
2016-01-22 15:11:01 +0900

    장바구니 수정완료

--- AnythingShopApp/src/main/java/com/anyshop/service/service/impl/MainServiceImpl.java
+++ AnythingShopApp/src/main/java/com/anyshop/service/service/impl/MainServiceImpl.java
@@ -44,7 +44,6 @@ public class MainServiceImpl implements MainService {
 		Product prod = (Product) session.getAttribute("prod");
 		int quentity = Integer.valueOf(request.getParameter("quentity"));
 		int check = 0;
-		int result = 0;
 		prod.setQuantity(quentity);
 		if (pList.size() == 0) {
 			pList.add(prod);
@@ -52,6 +51,7 @@ public class MainServiceImpl implements MainService {
 			for (int i = 0; i < pList.size(); i++) {
 				if (prod.getpNo() == pList.get(i).getpNo()) {
 					check+=1;
+					pList.get(i).setQuantity(quentity);
 				} 
 			}
 			if(check==0){

0db65d4eed1c54593c0153e4a5c8db507010165a
delight26 <ljb0426@naver.com>
2016-01-25 12:36:19 +0900

    최근 본 상품 구현 완료

--- AnythingShopApp/src/main/java/com/anyshop/service/impl/MainServiceImpl.java
+++ AnythingShopApp/src/main/java/com/anyshop/service/impl/MainServiceImpl.java
@@ -1,9 +1,14 @@
 package com.anyshop.service.impl;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +23,7 @@ public class MainServiceImpl implements MainService {
 
 	private MainDao mainDao;
 	private List<Product> pList = new ArrayList<Product>();
+	private List<Product> cookieList = new ArrayList<Product>();
 
 	@Autowired
 	public void setMainDao(MainDao mainDao) {
@@ -32,11 +38,30 @@ public class MainServiceImpl implements MainService {
 	}
 
 	@Override
-	public void getProductDetail(HttpServletRequest request) {
+	public void getProductDetail(HttpServletRequest request, HttpServletResponse response) {
 		int pNo = Integer.valueOf(request.getParameter("pNo"));
 		Product prod = mainDao.getProductDetail(pNo);
 
 		request.setAttribute("prod", prod);
+
+		try {
+			Cookie pname = new Cookie("prodname", URLEncoder.encode(prod.getName(), "utf-8"));
+			pname.setMaxAge(60 * 60 * 24);
+			Cookie pimage = new Cookie("prodimage", URLEncoder.encode(prod.getImage(), "utf-8"));
+			pimage.setMaxAge(60 * 60 * 24);
+			Cookie pprice = new Cookie("prodprice", URLEncoder.encode(Integer.toString(prod.getPrice()), "utf-8"));
+			pprice.setMaxAge(60 * 60 * 24);
+			Cookie pnoprice = new Cookie("prodprice", URLEncoder.encode(Integer.toString(prod.getpNo()), "utf-8"));
+			pprice.setMaxAge(60 * 60 * 24);
+
+			response.addCookie(pname);
+			response.addCookie(pimage);
+			response.addCookie(pprice);
+			addWatchProduct(pname, pimage, pprice, pnoprice);
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	@Override
@@ -50,19 +75,48 @@ public class MainServiceImpl implements MainService {
 		} else {
 			for (int i = 0; i < pList.size(); i++) {
 				if (prod.getpNo() == pList.get(i).getpNo()) {
-					check+=1;
+					check += 1;
 					pList.get(i).setQuantity(quentity);
-				} 
+				}
 			}
-			if(check==0){
+			if (check == 0) {
 				pList.add(prod);
 			}
 		}
 		request.setAttribute("pList", pList);
 	}
-	
+
 	@Override
 	public void getCart(HttpServletRequest request) {
 		request.setAttribute("pList", pList);
+	}
+
+	// 쿠키 리스트에 담기
+	@Override
+	public void addWatchProduct(Cookie nCookie, Cookie iCookie, Cookie pCookie, Cookie pnoCookie) {
+		Product p = new Product();
+		try {
+			p.setName(URLDecoder.decode(nCookie.getValue(), "utf-8"));
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		p.setImage(iCookie.getValue());
+		p.setPrice(Integer.parseInt(pCookie.getValue()));
+		p.setpNo(Integer.parseInt(pnoCookie.getValue()));
+		int count=0;
+		for (int i = 0; i < cookieList.size(); i++){
+			if(cookieList.get(i).getpNo()==p.getpNo()){
+				count++;
+			}
+		}
+		if (count == 0) {
+			cookieList.add(p);
+		}
+			
+	}
+
+	@Override
+	public void getWatchProduct(HttpServletRequest request) {
+		request.setAttribute("cookieList", cookieList);
 	}
 }
---
+++ AnythingShopApp/src/main/webapp/WEB-INF/views/member/recentWatch.jsp
@@ -0,0 +1,39 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<script>
+function detail(pNo){
+	parent.document.location.href="proddetail?pNo="+pNo;
+}
+</script>
+</head>
+<body>
+<div>
+		<table>
+			<tr>
+				<td><img src="resources/images/title_orderlist.gif" /></td>
+			</tr>
+			<tr>
+				<td>HOME > 최근본상품</td>
+			</tr>
+		</table>
+		<table>
+		<c:forEach var="c" items="${ cookieList }">
+		
+				<tr>
+					<td><a href="javascript:detail(${c.pNo })">
+					<img src="resources/images/${c.image }" style="width:100px;height:100px"/></a></td>
+					<td>${c.name }</td>
+					<td>${c.price }</td>
+				</tr>
+				
+			</c:forEach>
+			</table>
+	</div>
+</body>
+</html>
